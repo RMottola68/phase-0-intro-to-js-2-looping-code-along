32f1ef47b955efa3dd2da5c495d31604b55fc2f2
Ryan Mottola <rmottola68@gmail.com>
2022-06-07 19:38:35 -0400

    commit tree 4b4fcf8e76e5c2cf11687bfd12b538998462f8f6

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+let writeCards = (arr, ev) => {
+    let newArr = [];
+    for(let i = 0; i < arr.length; i++ ) {
+        newArr.push(`Thank you, ${arr[i]}, for the wonderful ${ev} gift!`)
+    }
+    return newArr;
+}
+
+let countDown = (num) => {
+    while(num >= 0) {
+        console.log(num)
+        num--
+    }
+}
+
+countDown(5);
